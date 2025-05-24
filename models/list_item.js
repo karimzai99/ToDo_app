8d02cfc77ee6d433a04eba441dfe27f1e588222d
Naseer Karimzai <karimzai99@gmail.com>
2025-05-24 18:15:14 -0400

    done the new and create routes

---
+++ models/list_item.js
@@ -0,0 +1,14 @@
+const list = [
+  {
+    toDo: "homework",
+    description: "have to done my home work",
+    isDone: true,
+  },
+  {
+    toDo: "wash dishes",
+    description: "need to finishe it by tonight",
+    isDone: false,
+  },
+];
+
+module.exports = list;
--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 const express = require("express");
 const app = express();
 
+// data
+const lists = require("./models/list_item.js");
+const list = require("./models/list_item.js");
+
 // MIDDLE WARE
 require("dotenv").config();
 const port = process.env.PORT || 3000;
+app.use(express.static("public")); // publlic folder connected
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //
 app.get("/", (req, res) => {
-  res.send(`to do list application`);
+  res.send(list);
+});
+
+// INDEX route
+app.get("/todo", (req, res) => {
+  res.render("index.ejs", { lists });
+});
+
+// new route
+app.get("/todo/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// Create route
+app.post("/todo", (req, res) => {
+  req.body.isDone = req.body.isDone === "on" ? true : false;
+  list.push(req.body);
+
+  res.redirect("/todo");
+});
+
+// show route
+app.get("/todo/:index", (req, res) => {
+  //   res.send(req.params.index);
+  const list = lists[req.params.index];
+  res.render("view.ejs", { list });
 });
 
 app.listen(port, (req, res) => {
-  console.log(`app is working at port${port}`);
+  console.log(`app is working at port ${port}`);
 });
