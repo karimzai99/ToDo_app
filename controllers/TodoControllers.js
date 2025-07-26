d5938f8c302d7f44bae4009b4c77a7bfa98c0a7e
Naseer Karimzai <karimzai99@gmail.com>
2025-07-26 02:52:49 -0400

    fixed controllers

---
+++ controllers/TodoControllers.js
@@ -0,0 +1,63 @@
+// 1. database connection
+
+// express
+const express = require("express");
+// router
+const router = express.Router();
+// data
+const lists = require("../models/list_item.js");
+const list = require("../models/list_item.js");
+
+//
+// router.get("/", (req, res) => {
+//   res.send(list);
+// });
+
+// INDEX route
+router.get("/", (req, res) => {
+  res.render("index.ejs", { lists });
+});
+
+// new route
+router.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// DELETE route
+router.delete("/:id", (req, res) => {
+  lists.splice(req.params.id, 1);
+  res.redirect("/todo");
+});
+
+// update route
+router.put("/:id", (req, res) => {
+  lists[req.params.id].isDone = !lists[req.params.id].isDone;
+  res.redirect("/todo");
+});
+
+// Create route
+router.post("/", (req, res) => {
+  req.body.isDone = req.body.isDone === "on" ? true : false;
+  list.push(req.body);
+
+  res.redirect("/todo");
+});
+
+// edit route
+
+router.get("/:index/edit", (req, res) => {
+  res.render("edit.ejs", {
+    list: lists[req.params.index],
+    index: req.params.index,
+  });
+});
+
+// show route
+router.get("/:index", (req, res) => {
+  //   res.send(req.params.index);
+  const list = lists[req.params.index];
+  const parm = req.params.index;
+  res.render("view.ejs", { list, parm });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 const methodOverRide = require("method-override");
+const todoControllers = require("./controllers/TodoControllers.js");
 
 // data
-const lists = require("./models/list_item.js");
-const list = require("./models/list_item.js");
 
 // MIDDLE WARE
 require("dotenv").config();
@@ -15,57 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverRide("_method"));
 
-//
-app.get("/", (req, res) => {
-  res.send(list);
-});
-
-// INDEX route
-app.get("/todo", (req, res) => {
-  res.render("index.ejs", { lists });
-});
-
-// new route
-app.get("/todo/new", (req, res) => {
-  res.render("new.ejs");
-});
-
-// DELETE route
-app.delete("/todo/:index", (req, res) => {
-  lists.splice(req.params.index, 1);
-  res.redirect("/todo");
-});
-
-// update route
-app.put("/todo/:khar", (req, res) => {
-  lists[req.params.khar].isDone = !lists[req.params.khar].isDone;
-  res.redirect("/todo");
-});
-
-// Create route
-app.post("/todo", (req, res) => {
-  req.body.isDone = req.body.isDone === "on" ? true : false;
-  list.push(req.body);
-
-  res.redirect("/todo");
-});
-
-// edit route
-
-app.get("/todo/:index/edit", (req, res) => {
-  res.render("edit.ejs", {
-    list: lists[req.params.index],
-    index: req.params.index,
-  });
-});
-
-// show route
-app.get("/todo/:index", (req, res) => {
-  //   res.send(req.params.index);
-  const list = lists[req.params.index];
-  const parm = req.params.index;
-  res.render("view.ejs", { list, parm });
-});
+// routes
+app.use("/todo", todoControllers);
 
 app.listen(port, (req, res) => {
   console.log(`app is working at port ${port}`);
